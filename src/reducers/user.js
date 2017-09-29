b2deef98ecbbc5c226358a37afadee3dabdb5d97
mustang85 <sergeant-kir@mail.ru>
2017-09-29 18:33:19 +0300

    added ConfirmEmailMessages and more..

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,6 @@
 import { USER_LOGGED_IN, USER_LOGGED_OUT } from '../types';
 
 export default function user(state = {}, action = {}) {
-  console.log('state', state);
   switch(action.type) {
     case USER_LOGGED_IN:
       return action.user;
