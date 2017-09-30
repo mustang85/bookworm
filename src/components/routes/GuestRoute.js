f1a9740943ab15fb1032804a5d4d362fbf8f6b1a
mustang85 <sergeant-kir@mail.ru>
2017-09-30 11:22:01 +0300

    Authorized routes

--- src/components/routes/GuestRoute.js
+++ src/components/routes/GuestRoute.js
@@ -3,19 +3,13 @@ import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const UserRoute = ({ isAuthenticated, component: Component, ...rest }) => {
-	console.log('UserRoute rest', rest);
-  return (
-  	<Route 
-  		{...rest} 
-  		render={props => 
-  			isAuthenticated ? <Component {...props} /> : <Redirect to="/" />}
-  	/>
-  );
+const GuestRoute = ({ isAuthenticated, component: Component, ...rest }) => {
+	console.log('GuestRoute rest', rest);
+  return <Route {...rest} render={props => !isAuthenticated ? <Component {...props} /> : <Redirect to="/dashboard" /> } />
 }
 
 
-UserRoute.propTypes = {
+GuestRoute.propTypes = {
   component: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired
 };
@@ -26,6 +20,6 @@ function mapStateToProps(state) {
 	}
 }
 
-export default connect(mapStateToProps)(UserRoute);
+export default connect(mapStateToProps)(GuestRoute);
 
 
