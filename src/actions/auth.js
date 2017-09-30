2af1adf57012f73d3300460b6e9783869375b4a1
mustang85 <sergeant-kir@mail.ru>
2017-09-30 21:02:24 +0300

    ConfirmationPage and Mongo Compass

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -18,6 +18,11 @@ export const login = credentials => dispatch =>
 
 export const logout = () => dispatch => {
   localStorage.removeItem('bookwormJWT');
-  dispatch(userLoggedOut())
+  dispatch(userLoggedOut());
 }
 
+export const confirm = token => dispatch =>
+	api.user.confirm(token).then(user => {
+		localStorage.bookwormJWT = user.token;
+		dispatch(userLoggedIn(user));
+	})
